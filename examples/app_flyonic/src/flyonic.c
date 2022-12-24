f9ed12a897dc2ec6d426adb20d4c7093b7714ea2
finn sueberkrueb <git@rpex.de>
2022-12-24 10:32:00 +0100

    changed Crazyflie to SPI Slave

--- examples/app_flyonic/src/flyonic.c
+++ examples/app_flyonic/src/flyonic.c
@@ -20,6 +20,7 @@
 #include "task.h"
 
 #include "deck.h"
+#include "deck_core.h"
 
 #define DEBUG_MODULE "Flyonic"
 #include "debug.h"
@@ -28,87 +29,45 @@
 
 #define BUFFER_SIZE 56
 #define EPS32_CS_PIN DECK_GPIO_IO4
-#define EPS32_HANDSHAKE_PIN DECK_GPIO_IO3
 
 
 
 
-static t_externalState sendState;
+//static t_externalState sendState;
 //static t_extrenalActuator reciveAction;
-static t_externalState reciveAction;
+//static t_externalState reciveAction;
 
 
   //t_externalState *reciveActionPtr = malloc(sizeof(t_externalState));
-//static uint8_t spiTxBuffer[BUFFER_SIZE];
-//static uint8_t spiRxBuffer[BUFFER_SIZE];
+static uint8_t spiTxBuffer[BUFFER_SIZE];
+static uint8_t spiRxBuffer[BUFFER_SIZE];
 static uint16_t spiSpeed = SPI_BAUDRATE_2MHZ; // SPI_BAUDRATE_21MHZ
 
 
 
 void appMain() {
 
-  pinMode(EPS32_CS_PIN, OUTPUT);
-  pinMode(EPS32_HANDSHAKE_PIN, INPUT);
-  spiBegin();
+  pinMode(EPS32_CS_PIN, INPUT);
+  spiBeginSlave();
 
-  //memset(&spiTxBuffer, 0x02, BUFFER_SIZE);
-  //memset(&spiRxBuffer, 0x03, BUFFER_SIZE);
-
-  bool send_next = true;
-  uint64_t startetime = usecTimestamp();
+  memset(&spiTxBuffer, 0x02, BUFFER_SIZE);
+  memset(&spiRxBuffer, 0x03, BUFFER_SIZE);
 
 
-  
-
-
-
-  spiBeginTransaction(spiSpeed);
+  spiBeginTransactionSlave(spiSpeed);
   DEBUG_PRINT("SPI for ESP32 sarted\n");
   while(1) {
     
-
-    if(send_next){
-      startetime = usecTimestamp();
-      sendState.timestamp = startetime;
-
-      send_next = false;
-    }
-
-
-
-    //DEBUG_PRINT("send: %d %d\n", spiTxBuffer[0], spiTxBuffer[1]);
-
-
-    if(digitalRead(EPS32_HANDSHAKE_PIN) == HIGH){
-      DEBUG_PRINT("Handshake high\n");
-    } else {
-      DEBUG_PRINT("Handshake low\n");
-    }
-
     
-    digitalWrite(EPS32_CS_PIN, LOW);
-    spiExchange(sizeof(t_externalState), (uint8_t *)(&sendState), (uint8_t *)(&reciveAction));
-    digitalWrite(EPS32_CS_PIN, HIGH);
+    //spiExchangeSlave(sizeof(t_externalState), (uint8_t *)(&sendState), (uint8_t *)(&reciveAction));
+    spiExchangeSlave(BUFFER_SIZE, (uint8_t *)(&spiTxBuffer), (uint8_t *)(&spiRxBuffer));
+    DEBUG_PRINT("recived: %d %d %d %d\n", spiRxBuffer[0], spiRxBuffer[1], spiRxBuffer[2], spiRxBuffer[3]);
+    DEBUG_PRINT("send: %d %d %d %d\n", spiTxBuffer[0], spiTxBuffer[1], spiTxBuffer[2], spiTxBuffer[3]);
+    
+    //spiTxBuffer
+    memcpy(&spiTxBuffer, &spiRxBuffer, BUFFER_SIZE);
+    
 
-
-    vTaskDelay(M2T(1));
-
-    //itoa(sendState.timestamp,spiTxBuffer,2);
-    //DEBUG_PRINT("%s (send)\n", spiTxBuffer); 
-
-    //itoa(reciveAction.timestamp,spiRxBuffer,2);
-    //DEBUG_PRINT("%s (recived)\n", spiRxBuffer);
-
-
-    if(reciveAction.timestamp == startetime){
-      uint64_t stoptime = usecTimestamp(); 
-      DEBUG_PRINT("latency in ms %.3f (send)\n", (stoptime - reciveAction.timestamp)/1000.0); 
-      DEBUG_PRINT("latency in ms %.3f\n", (stoptime - startetime)/1000.0);
-      send_next = true;
-      vTaskDelay(M2T(500));
-    }
-
-    //DEBUG_PRINT("recived: %d %d\n", spiRxBuffer[0], spiRxBuffer[1]);
   }
 
   spiEndTransaction();
--- src/deck/api/deck_spi.c
+++ src/deck/api/deck_spi.c
@@ -78,6 +78,16 @@
 #define SPI_MOSI_SOURCE         GPIO_PinSource7
 #define SPI_MOSI_AF             GPIO_AF_SPI1
 
+#define SPI_TX_DMA_STREAM_SLAVE       	DMA2_Stream5
+#define SPI_TX_DMA_IRQ_SLAVE          	DMA2_Stream5_IRQn
+#define SPI_TX_DMA_CHANNEL_SLAVE      	DMA_Channel_3
+#define SPI_TX_DMA_FLAG_TCIF_SLAVE    	DMA_FLAG_TCIF5
+
+#define SPI_RX_DMA_STREAM_SLAVE       	DMA2_Stream0
+#define SPI_RX_DMA_IRQ_SLAVE          	DMA2_Stream0_IRQn
+#define SPI_RX_DMA_CHANNEL_SLAVE      	DMA_Channel_3
+#define SPI_RX_DMA_FLAG_TCIF_SLAVE    	DMA_FLAG_TCIF0
+
 
 #define DUMMY_BYTE         0xA5
 
@@ -89,7 +99,8 @@ static SemaphoreHandle_t spiMutex;
 
 static void spiDMAInit();
 static void spiConfigureWithSpeed(uint16_t baudRatePrescaler);
-
+static void spiDMAInitSlave();
+static void spiConfigureWithSpeedSlave(uint16_t baudRatePrescaler);
 
 void spiBegin(void)
 {
@@ -148,6 +159,60 @@ void spiBegin(void)
   isInit = true;
 }
 
+void spiBeginSlave(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  // binary semaphores created using xSemaphoreCreateBinary() are created in a state
+  // such that the the semaphore must first be 'given' before it can be 'taken'
+  txComplete = xSemaphoreCreateBinary();
+  rxComplete = xSemaphoreCreateBinary();
+  spiMutex = xSemaphoreCreateMutex();
+
+  /*!< Enable the SPI clock */
+  SPI_CLK_INIT(SPI_CLK, ENABLE);
+
+  /*!< Enable GPIO clocks */
+  RCC_AHB1PeriphClockCmd(SPI_SCK_GPIO_CLK | SPI_MISO_GPIO_CLK |
+                         SPI_MOSI_GPIO_CLK, ENABLE);
+
+  /*!< Enable DMA Clocks */
+  SPI_DMA_CLK_INIT(SPI_DMA_CLK, ENABLE);
+
+  /*!< SPI pins configuration *************************************************/
+
+  /*!< Connect SPI pins to AF5 */
+  GPIO_PinAFConfig(SPI_SCK_GPIO_PORT, SPI_SCK_SOURCE, SPI_SCK_AF);
+  GPIO_PinAFConfig(SPI_MISO_GPIO_PORT, SPI_MISO_SOURCE, SPI_MISO_AF);
+  GPIO_PinAFConfig(SPI_MOSI_GPIO_PORT, SPI_MOSI_SOURCE, SPI_MOSI_AF);
+
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN;
+
+  /*!< SPI SCK pin configuration */
+  GPIO_InitStructure.GPIO_Pin = SPI_SCK_PIN;
+  GPIO_Init(SPI_SCK_GPIO_PORT, &GPIO_InitStructure);
+
+  /*!< SPI MOSI pin configuration */
+  GPIO_InitStructure.GPIO_Pin =  SPI_MOSI_PIN;
+  GPIO_Init(SPI_MOSI_GPIO_PORT, &GPIO_InitStructure);
+
+  /*!< SPI MISO pin configuration */
+  GPIO_InitStructure.GPIO_Pin =  SPI_MISO_PIN;
+  GPIO_Init(SPI_MISO_GPIO_PORT, &GPIO_InitStructure);
+
+  /*!< SPI DMA Initialization */
+  spiDMAInitSlave();
+
+  /*!< SPI configuration */
+  spiConfigureWithSpeedSlave(SPI_BAUDRATE_2MHZ);
+
+  isInit = true;
+}
+
 static void spiDMAInit()
 {
   DMA_InitTypeDef  DMA_InitStructure;
@@ -192,6 +257,51 @@ static void spiDMAInit()
   NVIC_Init(&NVIC_InitStructure);
 }
 
+static void spiDMAInitSlave()
+{
+  DMA_InitTypeDef  DMA_InitStructure;
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  /* Configure DMA Initialization Structure */
+  DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable ;
+  DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_1QuarterFull ;
+  DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single ;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+  DMA_InitStructure.DMA_PeripheralBaseAddr =(uint32_t) (&(SPI->DR)) ;
+  DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_BufferSize = 0; // set later
+  DMA_InitStructure.DMA_Memory0BaseAddr = 0; // set later
+
+  // Configure TX DMA
+  DMA_InitStructure.DMA_Channel = SPI_TX_DMA_CHANNEL_SLAVE;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+  DMA_Cmd(SPI_TX_DMA_STREAM_SLAVE,DISABLE);
+  DMA_Init(SPI_TX_DMA_STREAM_SLAVE, &DMA_InitStructure);
+
+  // Configure RX DMA
+  DMA_InitStructure.DMA_Channel = SPI_RX_DMA_CHANNEL_SLAVE;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
+  DMA_Cmd(SPI_RX_DMA_STREAM_SLAVE,DISABLE);
+  DMA_Init(SPI_RX_DMA_STREAM_SLAVE, &DMA_InitStructure);
+
+  // Configure interrupts
+  // TODO: Finn maybe even NVIC_VERY_HIGH_PRI ???
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = NVIC_HIGH_PRI;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+
+  NVIC_InitStructure.NVIC_IRQChannel = SPI_TX_DMA_IRQ_SLAVE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  NVIC_InitStructure.NVIC_IRQChannel = SPI_RX_DMA_IRQ_SLAVE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+
 static void spiConfigureWithSpeed(uint16_t baudRatePrescaler)
 {
   SPI_InitTypeDef  SPI_InitStructure;
@@ -208,6 +318,26 @@ static void spiConfigureWithSpeed(uint16_t baudRatePrescaler)
   SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
   SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
 #endif
+  SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
+  SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
+  SPI_InitStructure.SPI_CRCPolynomial = 0; // Not used
+
+  SPI_InitStructure.SPI_BaudRatePrescaler = baudRatePrescaler;
+  SPI_Init(SPI, &SPI_InitStructure);
+}
+
+// TODO: Finn optimize SPI settings for speed
+static void spiConfigureWithSpeedSlave(uint16_t baudRatePrescaler)
+{
+  SPI_InitTypeDef  SPI_InitStructure;
+
+  SPI_I2S_DeInit(SPI);
+
+  SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+  SPI_InitStructure.SPI_Mode = SPI_Mode_Slave;
+  SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
+  SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
+  SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
   SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
   SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
   SPI_InitStructure.SPI_CRCPolynomial = 0; // Not used
@@ -261,13 +391,61 @@ bool spiExchange(size_t length, const uint8_t * data_tx, uint8_t * data_rx)
   return result;
 }
 
+bool spiExchangeSlave(size_t length, const uint8_t * data_tx, uint8_t * data_rx)
+{
+  // DMA already configured, just need to set memory addresses
+  SPI_TX_DMA_STREAM_SLAVE->M0AR = (uint32_t)data_tx;
+  SPI_TX_DMA_STREAM_SLAVE->NDTR = length;
+
+  SPI_RX_DMA_STREAM_SLAVE->M0AR = (uint32_t)data_rx;
+  SPI_RX_DMA_STREAM_SLAVE->NDTR = length;
+
+  // Enable SPI DMA Interrupts
+  DMA_ITConfig(SPI_TX_DMA_STREAM_SLAVE, DMA_IT_TC, ENABLE);
+  DMA_ITConfig(SPI_RX_DMA_STREAM_SLAVE, DMA_IT_TC, ENABLE);
+
+  // Clear DMA Flags
+  DMA_ClearFlag(SPI_TX_DMA_STREAM_SLAVE, DMA_FLAG_FEIF5|DMA_FLAG_DMEIF5|DMA_FLAG_TEIF5|DMA_FLAG_HTIF5|DMA_FLAG_TCIF5);
+  DMA_ClearFlag(SPI_RX_DMA_STREAM_SLAVE, DMA_FLAG_FEIF0|DMA_FLAG_DMEIF0|DMA_FLAG_TEIF0|DMA_FLAG_HTIF0|DMA_FLAG_TCIF0);
+
+  // Enable DMA Streams
+  DMA_Cmd(SPI_TX_DMA_STREAM_SLAVE,ENABLE);
+  DMA_Cmd(SPI_RX_DMA_STREAM_SLAVE,ENABLE);
+
+  // Enable SPI DMA requests
+  SPI_I2S_DMACmd(SPI, SPI_I2S_DMAReq_Tx, ENABLE);
+  SPI_I2S_DMACmd(SPI, SPI_I2S_DMAReq_Rx, ENABLE);
+
+  // Enable peripheral
+  SPI_Cmd(SPI, ENABLE);
+
+  // Wait for completion
+  bool result = (xSemaphoreTake(txComplete, portMAX_DELAY) == pdTRUE)
+             && (xSemaphoreTake(rxComplete, portMAX_DELAY) == pdTRUE);
+
+  // Disable peripheral
+  SPI_Cmd(SPI, DISABLE);
+  return result;
+}
+
 void spiBeginTransaction(uint16_t baudRatePrescaler)
 {
   xSemaphoreTake(spiMutex, portMAX_DELAY);
   spiConfigureWithSpeed(baudRatePrescaler);
 }
 
+void spiBeginTransactionSlave(uint16_t baudRatePrescaler)
+{
+  xSemaphoreTake(spiMutex, portMAX_DELAY);
+  spiConfigureWithSpeedSlave(baudRatePrescaler);
+}
+
 void spiEndTransaction()
+{
+  xSemaphoreGive(spiMutex);
+}
+
+void spiEndTransactionSlave()
 {
   xSemaphoreGive(spiMutex);
 }
